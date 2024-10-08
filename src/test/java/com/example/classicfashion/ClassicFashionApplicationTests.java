0af08165990048cfaa5cc78200f562d7503d1e3b
TRANTHAO2406 <tranthao24061999@gmail.com>
2024-10-08 11:34:19 +0700

    hello

---
+++ src/test/java/com/example/classicfashion/ClassicFashionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.classicfashion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassicFashionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
