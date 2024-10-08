143a169d0e29036271d17bc2799c3a9ffd1f38a8
HuanNguyen01 <huannguyen199938@gmail.com>
2024-10-08 17:07:29 +0700

    huann

--- src/main/java/com/example/classicfashion/model/Image.java
+++ src/main/java/com/example/classicfashion/model/Image.java
@@ -1,10 +1,16 @@
 package com.example.classicfashion.model;
 
 import java.time.LocalDate;
+import java.util.Set;
 
-import java.util.List;
-
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "Image")
