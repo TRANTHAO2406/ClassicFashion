1fe68e699e93435efa692e0d5287db0bcd68a5c3
TRANTHAO2406 <tranthao24061999@gmail.com>
2024-10-24 16:58:25 +0700

    edit bug user task

--- src/main/java/com/example/classicfashion/controller/AdminController.java
+++ src/main/java/com/example/classicfashion/controller/AdminController.java
@@ -1,37 +1,47 @@
 package com.example.classicfashion.controller;
 
-import com.example.classicfashion.model.Category;
-import com.example.classicfashion.service.CategoryService;
+import com.example.classicfashion.model.Users;
 import com.example.classicfashion.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
     private final UserService userService;
-    private final CategoryService categoryService;
     @Autowired
-    public AdminController(UserService userService, CategoryService categoryService) {
+    public AdminController(UserService userService) {
         this.userService = userService;
-        this.categoryService = categoryService;
     }
 
-    @GetMapping("/delete-user/{id}")
-    public String deleteUser(@PathVariable Long id){
-        userService.deleteUserById(id);
-        return "redirect:admin/user-list";
-    }
+    @GetMapping("/customer-list")
+    public String showCategoryList(Model model,
+                                   @RequestParam(defaultValue = "0") int page,
+                                   @RequestParam(defaultValue = "5") int size,
+                                   @RequestParam(required = false) String keyword) {
+        PageRequest pageable = PageRequest.of(page, size);
+        Page<Users> usersPage;
+        if(keyword != null && !keyword.isEmpty()){
+            usersPage = userService.searchUsers(keyword, pageable);
+            model.addAttribute("keyword", keyword);
 
-    @GetMapping("/user-list")
-    public String showUserList(Model model){
-        model.addAttribute("user", userService.getAllUsers());
-        return "admin/user-list";
+            if(usersPage.isEmpty()){
+                model.addAttribute("message", "Không có khách hàng nào với tên: " + keyword);
+            }
+        } else {
+            usersPage = userService.getAllUsers(pageable);
+        }
+        model.addAttribute("users", usersPage.getContent());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", usersPage.getTotalPages());
+        return "admin/customer-list";
     }
 
     @GetMapping
